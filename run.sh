dca6425b8dec8c50cc58f588a79aa0f5dce51ba4
qzsnitz <qzsnitz@yeah.net>
2016-04-30 00:15:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
+    echo "=> Found authorized keys"
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+    IFS=$'\n'
+    arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
+    for x in $arr
+    do
+        x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
+        cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
+            echo "$x" >> /root/.ssh/authorized_keys
+        fi
+    done
+fi
+
+if [ ! -f /.root_pw_set ]; then
+	/set_root_pw.sh
+fi
+
 VOLUME_HOME="/var/lib/mysql"
 
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
@@ -14,4 +37,5 @@ else
     echo "=> Using an existing volume of MySQL"
 fi
 
+exec /usr/sbin/sshd -D
 exec supervisord -n
